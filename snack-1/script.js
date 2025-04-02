13887a1e332bf13c176bfb245f1bc72179522860
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-04-02 15:06:05 +0200

    [UPD] Snack 1 done

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,6 +2,9 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
-for (let i = 0; i<names.length; i++){
-    console.log(names[i]);
-}
+// for (let i = 0; i<names.length; i++){
+//     console.log(names[i]);
+// }
+names.forEach((ciao) => {
+    console.log(ciao);
+});
