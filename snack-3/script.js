b8e1f9e71d55ed567aeb50b65026d2bb056427e7
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-04-02 15:27:14 +0200

    [UPD] snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,11 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+// for (let i = 0; i < numbers.length; i++) {
+//   numbers[i] += 1;
+// }
+// console.log(numbers);`
+const newNumbers = numbers.map((number) => {
+  return number + 1;
+});
+console.log(newNumbers);
