43b936a18127de979488e8c89b543dc52d958704
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-04-02 15:32:32 +0200

    [UPD] snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,10 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+// for (let i = 0; i<posts.length; i++){
+//   console.log(posts[i].author);
+// }
+const authors = posts.map((post) => {
+  return post.author;
+});
+console.log(authors);
